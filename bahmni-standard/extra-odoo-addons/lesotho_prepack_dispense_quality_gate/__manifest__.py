f5b2b579c6bb28cc25477ddc70b9f9f22b752170
Khabo Morolong <khabomorolong@gmail.com>
2025-12-15 15:59:18 +0200

    Added Quality Checks

---
+++ bahmni-standard/extra-odoo-addons/lesotho_prepack_dispense_quality_gate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Lesotho Prepacking & Dispensing Quality Gate",
+    "version": "16.0.1.0.0",
+    "category": "Quality",
+    "summary": "Blocks prepacking (MRP) and dispensing until quality checks are passed.",
+    "author": "Lesotho MOH / Implementing Partner",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
+        "stock",
+        "quality_control_oca",
+        "quality_control_stock_oca",
+        "quality_control_mrp_oca",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ bahmni-standard/extra-odoo-addons/lesotho_prepack_dispense_quality_gate/models/__init__.py
@@ -0,0 +1,3 @@
+from . import qc_gate_utils
+from . import mrp_production
+from . import stock_picking
