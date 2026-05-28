07a6605a9a4ab766326080af0f108088ee30b728
lmphatsi <la.mphatsi@gmail.com>
2026-05-28 21:17:24 +0200

    Wire eLMIS integration addon

---
+++ bahmni-standard/extra-odoo-addons/elmis_integration/models/__init__.py
@@ -0,0 +1,6 @@
+from . import elmis_inventory_sync
+from . import elmis_inventory_sync_run
+from . import elmis_inventory_sync_wizard
+from . import product_product
+from . import res_config_settings
+from . import stock_lot
---
+++ bahmni-standard/extra-odoo-addons/elmis_integration/tests/__init__.py
@@ -0,0 +1,6 @@
+from . import test_elmis_inventory_sync
+from . import test_elmis_inventory_sync_run
+from . import test_elmis_inventory_sync_wizard
+from . import test_product_product
+from . import test_res_config_settings
+from . import test_stock_lot
